08002f163918675b012964247c5877013c3c4250
pjy <1736989646@qq.com>
2021-11-11 16:23:28 +0800

    window trigger

--- analyzer-alert/src/main/java/cloud/erda/analyzer/alert/Main.java
+++ analyzer-alert/src/main/java/cloud/erda/analyzer/alert/Main.java
@@ -248,8 +248,8 @@ public class Main {
         DataStream<AlertEvent> alertEventLevel = alertEventsWithTemplate
                 .assignTimestampsAndWatermarks(new AlertEventWatermarkExtractor())
                 .keyBy(new AlertEventRuleFilterFunction())
-//                .window(new AlertEventWindowAssigner())
-                .window(TumblingProcessingTimeWindows.of(Time.seconds(45)))
+                .window(new AlertEventWindowAssigner())
+//                .window(TumblingProcessingTimeWindows.of(Time.seconds(45)))
                 .process(new AlertLevelProcessFunction())
 //                .reduce(new AlertEventLevelReduceFunction())
 //                .reduce(new AlertEventLevelReduceFunction(),new AlertLevelProcessFunction())
