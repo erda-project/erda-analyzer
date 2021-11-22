e3a6189082be1ab9d8a3e26057a920fa699eef29
liuhaoyang <liuhaoyang1221@hotmail.com>
2021-11-22 16:33:30 +0800

    Fix ticket event missing bug (#82)
    * Fix ticket event missing bug
    
    * notes

--- analyzer-alert/src/main/java/cloud/erda/analyzer/alert/Main.java
+++ analyzer-alert/src/main/java/cloud/erda/analyzer/alert/Main.java
@@ -161,17 +161,12 @@ public class Main {
                 .setParallelism(parameterTool.getInt(STREAM_PARALLELISM_OPERATOR))
                 .name("broadcast alert notify");
 
-        DataStream<AlertEvent> levelMatchedAlertEventsWithNotify = alertEventsWithNotify
-                .filter(new AlertEventLevelFilterFunction())
-                .setParallelism(parameterTool.getInt(STREAM_PARALLELISM_OPERATOR))
-                .name("filter event notify level");
-
         DataStream<NotifyEvent> notifyEventWithTemplate = notifyEventDataStream.connect(allUniversalTemplates.broadcast(StateDescriptors.notifyTemplate))
                 .process(new NotifyTemplateProcessFunction(parameterTool.getLong(METRIC_METADATA_TTL, 7500), StateDescriptors.notifyTemplate))
                 .setParallelism(parameterTool.getInt(STREAM_PARALLELISM_OPERATOR))
                 .name("notify event with template");
 
-        DataStream<AlertEvent> alertEventsWithTemplate = levelMatchedAlertEventsWithNotify
+        DataStream<AlertEvent> alertEventsWithTemplate = alertEventsWithNotify
                 .connect(alertNotifyTemplateQuery.union(alertNotifyCustomTemplateQuery).broadcast(StateDescriptors.alertNotifyTemplateStateDescriptor))
                 .process(new AlertNotifyTemplateBroadcastProcessFunction(parameterTool.getLong(METRIC_METADATA_TTL,
                         75000), StateDescriptors.alertNotifyTemplateStateDescriptor))
@@ -243,6 +238,9 @@ public class Main {
         }
 
         DataStream<AlertEvent> alertEventLevel = alertEventsWithTemplate
+                .filter(new AlertEventLevelFilterFunction())
+                .setParallelism(parameterTool.getInt(STREAM_PARALLELISM_OPERATOR))
+                .name("filter event notify level")
                 .assignTimestampsAndWatermarks(new AlertEventWatermarkExtractor())
                 .keyBy(new AlertEventGroupFunction())
                 .window(TumblingProcessingTimeWindows.of(Time.seconds(15)))
